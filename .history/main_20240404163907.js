f50c2198a2ed45fafa7e844a6c4ad8857677e6c4
asa <abdullayefasadbek@gmail.com>
2024-04-14 21:40:34 +0500

    Buhruz proyect

---
+++ .history/main_20240404163907.js
@@ -0,0 +1,9 @@
+let a = prompt("How old are you")
+if ( a >= 14 &&   a < 50) {
+    console.log("Welcome to  my web site");
+    alert("You can enter to the page")
+} else {
+    console.log("I am sorry bro.You must grow up");
+    alert("You must grow up")
+    
+}
